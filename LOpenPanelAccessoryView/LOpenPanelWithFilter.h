e0664bdf255ac1217c8a1a98b0934915299055ca
AnarL <anar0615@sina.com>
2017-03-07 17:18:28 +0800

    Initial

---
+++ LOpenPanelAccessoryView/LOpenPanelWithFilter.h
@@ -0,0 +1,13 @@
+//
+//  LOpenPanelWithFilter.h
+//  LOpenPanelAccessoryView
+//
+//  Created by AnarLong on 2017/3/7.
+//  Copyright © 2017年 Digiarty. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LOpenPanelWithFilter : NSOpenPanel
+
+@end
---
+++ LOpenPanelAccessoryView/LOpenPanelWithFilter.m
@@ -0,0 +1,13 @@
+//
+//  LOpenPanelWithFilter.m
+//  LOpenPanelAccessoryView
+//
+//  Created by AnarLong on 2017/3/7.
+//  Copyright © 2017年 Digiarty. All rights reserved.
+//
+
+#import "LOpenPanelWithFilter.h"
+
+@implementation LOpenPanelWithFilter
+
+@end
